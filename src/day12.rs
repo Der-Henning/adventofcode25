565fbeb74f2a1699e274f7caf2d21fa1ffa3f148
Henning Merklinger <henning.merklinger@gmail.com>
2025-12-13 23:45:27 +0100

    clean up and comments for day12

--- src/day12.rs
+++ src/day12.rs
@@ -45,9 +45,10 @@ const TEST_DATA: &str = r#"
 "#;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, PartialOrd, Ord)]
-struct Tile([[bool; 3]; 3]);
+struct Present([[bool; 3]; 3]);
 
-impl Tile {
+impl Present {
+    // rotate present clockwise
     fn rotate(&self) -> Self {
         Self(
             (0..3)
@@ -64,6 +65,7 @@ impl Tile {
         )
     }
 
+    // get the number of cells in the region
     fn cell_count(&self) -> u32 {
         (0..3)
             .cartesian_product(0..3)
@@ -71,18 +73,20 @@ impl Tile {
             .sum()
     }
 
+    // Calculate all present rotations without symmetric duplicates
     fn rotations(&self) -> Vec<Self> {
-        let mut new_tiles = vec![*self];
+        let mut rotated_present = vec![*self];
         for _ in 0..3 {
-            new_tiles.push(new_tiles.last().unwrap().rotate())
+            rotated_present.push(rotated_present.last().unwrap().rotate())
         }
-        new_tiles.sort_unstable();
-        new_tiles.dedup();
-        new_tiles
+        rotated_present.sort_unstable();
+        rotated_present.dedup();
+        rotated_present
     }
 }
 
-impl Display for Tile {
+// Implement Display for debugging
+impl Display for Present {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for row in self.0 {
             for col in row {
@@ -98,7 +102,8 @@ impl Display for Tile {
     }
 }
 
-impl From<&str> for Tile {
+// Create Present from str input
+impl From<&str> for Present {
     fn from(value: &str) -> Self {
         Self(
             value
@@ -120,16 +125,18 @@ impl From<&str> for Tile {
 }
 
 #[derive(Debug, Clone)]
-struct Area(Vec<Vec<bool>>);
+struct Region(Vec<Vec<bool>>);
 
-impl From<&str> for Area {
+// Create Region from str input
+impl From<&str> for Region {
     fn from(value: &str) -> Self {
         let (x, y) = value.split_once('x').unwrap();
         Self(vec![vec![false; x.parse().unwrap()]; y.parse().unwrap()])
     }
 }
 
-impl Display for Area {
+// Implement display for debugging
+impl Display for Region {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for y in 0..self.0.len() {
             for x in 0..self.0[0].len() {
@@ -145,24 +152,26 @@ impl Display for Area {
     }
 }
 
-impl Area {
-    fn place(&self, tile: &Tile, x: usize, y: usize) -> Option<Self> {
-        let mut new_area = self.clone();
-        for i in 0..tile.0[0].len() {
-            for j in 0..tile.0.len() {
-                if tile.0[i][j] {
-                    if new_area.0[y + i][x + j] {
+impl Region {
+    // try to place a present a position (x, y)
+    // Return updated region when successful else None
+    fn place(&self, present: &Present, x: usize, y: usize) -> Option<Self> {
+        let mut updated_region = self.clone();
+        for i in 0..present.0[0].len() {
+            for j in 0..present.0.len() {
+                if present.0[i][j] {
+                    if updated_region.0[y + i][x + j] {
                         return None;
                     }
-                    new_area.0[y + i][x + j] = true;
+                    updated_region.0[y + i][x + j] = true;
                 }
             }
         }
-        Some(new_area)
+        Some(updated_region)
     }
 }
 
-type Data = (Vec<Vec<Tile>>, Vec<(Area, Vec<u32>)>);
+type Data = (Vec<Vec<Present>>, Vec<(Region, Vec<u32>)>);
 
 fn get_data() -> Data {
     let data_str = get_input(12).unwrap();
@@ -170,61 +179,74 @@ fn get_data() -> Data {
 
     let parts = data_str.split("\n\n");
 
-    let tiles = parts
+    // parse presents
+    let presents = parts
         .clone()
         .take(6)
-        .map(Tile::from)
+        .map(Present::from)
         .map(|t| t.rotations())
         .collect::<Vec<_>>();
 
-    let areas = parts
+    // parse regions
+    let regions = parts
         .last()
         .unwrap()
         .trim()
         .split("\n")
         .map(|s| {
             let (a, s) = s.split_once(": ").unwrap();
-            let area = Area::from(a);
+            let region = Region::from(a);
             let shapes = s.split_whitespace().map(|i| i.parse().unwrap()).collect();
-            (area, shapes)
+            (region, shapes)
         })
         .collect::<Vec<_>>();
 
-    (tiles, areas)
+    (presents, regions)
 }
 
-fn fill_area(
-    area: Area,
-    tile_counts: Vec<u32>,
-    tiles: &Vec<Vec<Tile>>,
+// Recursively place presents in region until all presents are positioned
+// Iterate over open slots in the region and try to place any present
+// When all presents are placed return true
+// Only search for a single solution
+fn place_presents(
+    region: Region,
+    present_counts: Vec<u32>,
+    presents: &Vec<Vec<Present>>,
     mut blocked_slots: HashSet<(usize, usize)>,
 ) -> Option<bool> {
-    (0..area.0[0].len() - 2)
-        .cartesian_product(0..area.0.len() - 2)
+    // Iterate over region slots
+    (0..region.0[0].len() - 2)
+        .cartesian_product(0..region.0.len() - 2)
         .filter_map(|(x, y)| {
-            if area.0[y][x] || blocked_slots.contains(&(x, y)) {
+            // Skip Slot, when already occupied or in set of blocked slots
+            if region.0[y][x] || blocked_slots.contains(&(x, y)) {
                 return None;
             }
-            match tile_counts
+            // Iterate over available presents
+            match present_counts
                 .iter()
                 .enumerate()
                 .filter_map(|(i, c)| {
                     if *c == 0 {
                         return None;
                     }
-                    tiles[i]
+                    // Iterate over present rotations
+                    presents[i]
                         .iter()
-                        .filter_map(|tile| {
-                            let mut tile_counts = tile_counts.clone();
-                            if let Some(new_area) = area.place(tile, x, y) {
-                                tile_counts[i] -= 1;
-                                if tile_counts.iter().sum::<u32>() == 0 {
+                        .filter_map(|present| {
+                            let mut present_counts = present_counts.clone();
+                            // Check if present can be placed
+                            // Update present counts and increase recursion level
+                            if let Some(new_region) = region.place(present, x, y) {
+                                present_counts[i] -= 1;
+                                if present_counts.iter().sum::<u32>() == 0 {
+                                    // recursion base case: All presents are placed
                                     return Some(true);
                                 }
-                                return fill_area(
-                                    new_area,
-                                    tile_counts,
-                                    tiles,
+                                return place_presents(
+                                    new_region,
+                                    present_counts,
+                                    presents,
                                     blocked_slots.clone(),
                                 );
                             }
@@ -236,6 +258,8 @@ fn fill_area(
             {
                 Some(b) => Some(b),
                 None => {
+                    // If no present can be placed in this slot,
+                    // cache this information for this branch
                     blocked_slots.insert((x, y));
                     None
                 }
@@ -245,21 +269,31 @@ fn fill_area(
 }
 
 pub fn part_1() {
-    let (tiles, areas) = get_data();
+    let (presents, regions) = get_data();
 
     let mut counter = 0;
-    for (area, tile_counts) in areas.iter() {
-        if tile_counts
+    for (region, present_counts) in regions.iter() {
+        // Early exit, when the number of presents exceeds the available area of the region
+        // Actually this completely solves the puzzle without all the recursion ...
+        if present_counts
             .iter()
             .enumerate()
-            .map(|(i, tile_count)| tile_count * tiles[i][0].cell_count())
+            .map(|(i, present_count)| present_count * presents[i][0].cell_count())
             .sum::<u32>()
-            > (area.0.len() * area.0[0].len()) as u32
+            > (region.0.len() * region.0[0].len()) as u32
         {
             continue;
         }
 
-        if fill_area(area.clone(), tile_counts.clone(), &tiles, HashSet::new()).is_some() {
+        if place_presents(
+            region.clone(),
+            present_counts.clone(),
+            &presents,
+            HashSet::new(),
+        )
+        .is_some()
+        {
+            // When found one solution increase counter
             counter += 1;
         }
     }
